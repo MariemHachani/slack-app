9abb3e034d30d8bb9dae87d94758a173a6e01058
mariem hacheni <hachanimariem16@gmail.com>
2021-02-23 15:31:03 +0100

    sidebar rooms design and firebase

--- src/firebase.js
+++ src/firebase.js
@@ -1,3 +1,6 @@
+import firebase from 'firebase';
+
+
 const firebaseConfig = {
     apiKey: "AIzaSyCp6TSViPgNZcd2Z_fTC_yy2M35Ik-8hRc",
     authDomain: "slack-dceb7.firebaseapp.com",
@@ -6,3 +9,10 @@ const firebaseConfig = {
     messagingSenderId: "1004905323393",
     appId: "1:1004905323393:web:8d4fe35e701d71535c2680"
 };
+
+const firebaseApp = firebase.initializeApp(firebaseConfig);
+const db = firebaseApp.firestore();
+const auth = firebase.auth();
+const provider = new firebase.auth.GoogleAuthProvider();
+
+export { auth, provider, db };
